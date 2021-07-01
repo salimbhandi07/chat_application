4722b79a30646349cd18e83f88cd9c88fac33200
salim <salimbhandi07@gmail.com>
2021-07-01 15:21:00 +0530

    chatty

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const app = require("express")();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server,{
+    cors:{ 
+        origin: "*",
+    },
+   
+});
+io.on("connection", (socket) =>{
+    console.log("what is socket: ", socket);
+    console.log("Socket is to active to be connection");
+
+    socket.on("chat",(payload)=>{
+        console.log("what is payload", payload);
+        io.emit("chat",payload)
+    })
+})
+
+server.listen(5000, ()=> {
+    console.log("Server is listening at port 5000...");
+})
+
